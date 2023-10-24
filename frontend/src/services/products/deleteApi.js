927430df22ac19942824e614a7b05adff2492461
AndreHilario <andre18hilario@gmail.com>
2023-10-24 18:37:50 -0300

    feat: add services to API response

---
+++ frontend/src/services/products/deleteApi.js
@@ -0,0 +1,6 @@
+import api from '../api';
+
+export async function deleteProductApi(id) {
+    const response = await api.delete(`/products/${id}`);
+    return response.data;
+}
---
+++ frontend/src/services/products/listApi.js
@@ -0,0 +1,6 @@
+import api from '../api';
+
+export async function listProductsApi() {
+    const response = await api.get(`/products`);
+    return response.data;
+}
