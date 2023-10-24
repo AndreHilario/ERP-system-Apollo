cc6fdc1709ff06927497fc6246956d81cfb17d9b
AndreHilario <andre18hilario@gmail.com>
2023-10-24 15:26:48 -0300

    fix: dotenv scripts and configs

--- backend/src/config/envs.ts
+++ backend/src/config/envs.ts
@@ -7,7 +7,7 @@ export function loadEnv() {
             ? ".env.test"
             : process.env.NODE_ENV === "development"
                 ? ".env.development"
-                : ".env.production";
+                : ".env";
 
     const currentEnvs = dotenv.config({ path });
     dotenvExpand.expand(currentEnvs);
